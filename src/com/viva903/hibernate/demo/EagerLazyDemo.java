f78059e6ac8143940b0926dafd40fab9309a2341
Jay_Lee <viva9032001@gmail.com>
2022-08-31 18:06:00 +0800

    add fetch type to learn about lazy/eager loading

---
+++ src/com/viva903/hibernate/demo/EagerLazyDemo.java
@@ -0,0 +1,50 @@
+package com.viva903.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.viva903.hibernate.demo.entity.Course;
+import com.viva903.hibernate.demo.entity.Instructor;
+import com.viva903.hibernate.demo.entity.InstructorDetails;
+
+public class EagerLazyDemo {
+
+	public static void main(String[] args) {
+
+//		create session factory
+		SessionFactory factory = new Configuration()
+									.configure("hibernate.cfg.xml")
+									.addAnnotatedClass(Instructor.class)
+									.addAnnotatedClass(InstructorDetails.class)
+									.addAnnotatedClass(Course.class)
+									.buildSessionFactory();
+//		create session
+		Session session = factory.getCurrentSession();
+
+		try {
+//			start a transaction
+			session.beginTransaction();
+			
+//			get instructor from db
+			int theIntructorId = 1;
+			Instructor tempInstructor = session.get(Instructor.class, theIntructorId);
+			
+			System.out.println("viva903 => Instructor : " + tempInstructor);
+//			fetch type : lazy, only retrieve courses on demand 
+			System.out.println("viva903 => Courses Associated : " + tempInstructor.getCourses());
+			
+//			commit transaction
+			session.getTransaction().commit();
+			System.out.println("viva903 => Done!");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			session.close();
+			factory.close();
+		}
+
+	}
+
+}
